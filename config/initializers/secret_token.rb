dca528a28ad0fb73f41c85f1145d9cf5a47da527
Daniel Daks <daks.daniel@gmail.com>
2013-01-10 18:28:25 -0500

    Installed Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LBuddy::Application.config.secret_token = '50abec0a741231a7602908e19f056bafba54b7af33a38a35d3c37071d9285997b94a5e04e9c33d9732e93b9ae16a943d9422f548ae0ab70e5d8a9ebc6d6cca49'
