185383d05c644f5c05222e6ae82953805477db1e
Daniel Daks <daks.daniel@gmail.com>
2013-02-19 11:24:05 -0600

    disabled javascript compression for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ LBuddy::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false #SWITCH THIS BACK TO TRUE AFTER DEBUG
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
